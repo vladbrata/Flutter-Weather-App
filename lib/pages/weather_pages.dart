518d036c50b504644caf21ccd29b3254e7f4f0a5
Vlad Brata <vlad.brata84@gmail.com>
2026-01-11 16:38:50 +0200

    made the api work

---
+++ lib/pages/weather_pages.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/models/weather_model.dart';
+import 'package:flutter_weather_app/service/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  _WeatherState createState() => _WeatherState();
+}
+
+class _WeatherState extends State<WeatherPage> {
+  final _weatherService = WeatherService(
+    apiKey: '004d62385622f114cb8a0cd1d1822f19',
+  );
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'loading cityyy...'),
+            Text('${_weather?.temperature.round()}°C'),
+          ],
+        ),
+      ),
+    );
+  }
+}
